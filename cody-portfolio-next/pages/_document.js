347ae4aa0de6fadd45fcf0172d7214ed9c04f650
GameProgrammerCody <gameprogrammercody@gmail.com>
2025-11-11 05:37:51 +0000

    Revert "Update _document.js"
    This reverts commit 1174baf9f92ae70ded6bda03f15ec997f1f501e9.

--- cody-portfolio-next/pages/_document.js
+++ cody-portfolio-next/pages/_document.js
@@ -1,14 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import SmartLink from '../src/components/SmartLink'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         {/* Favicon */}
-        <SmartLink rel="icon" type="image/png" href="/favicon.png" />
+        <Link rel="icon" type="image/png" href="/favicon.png" />
         {/* Optional: high-resolution Apple touch icon */}
-        <SmartLink rel="apple-touch-icon" href="/favicon.png" />
+        <Link rel="apple-touch-icon" href="/favicon.png" />
         <meta name="theme-color" content="#0ef" />
       </Head>
       <body>
