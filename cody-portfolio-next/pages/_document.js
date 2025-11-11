1174baf9f92ae70ded6bda03f15ec997f1f501e9
GameProgrammerCody <gameprogrammercody@gmail.com>
2025-11-11 05:30:42 +0000

    Update _document.js

--- cody-portfolio-next/pages/_document.js
+++ cody-portfolio-next/pages/_document.js
@@ -1,13 +1,14 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import SmartLink from '../src/components/SmartLink'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         {/* Favicon */}
-        <Link rel="icon" type="image/png" href="/favicon.png" />
+        <SmartLink rel="icon" type="image/png" href="/favicon.png" />
         {/* Optional: high-resolution Apple touch icon */}
-        <Link rel="apple-touch-icon" href="/favicon.png" />
+        <SmartLink rel="apple-touch-icon" href="/favicon.png" />
         <meta name="theme-color" content="#0ef" />
       </Head>
       <body>
